e31cdc772cfbe929d3d4530b03e8c4232cc97ad2
Dave Emmanuel Nuada <davenuada025@gmail.com>
2026-09-04 11:07:07 +0800

    Feat: Implement GET all subjects route

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,20 @@
 import express from "express";
+import subjectsRouter from "./routes/subjects.ts";
 
 const app = express();
 const PORT = process.env.PORT || 8000;
 
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  }),
+);
+
 app.use(express.json());
+
+app.use("/api/subjects", subjectsRouter);
 
 app.get("/", (req, res) => {
   res.send("Hello, Welcome to Classroom API!");
@@ -12,3 +23,10 @@ app.get("/", (req, res) => {
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
 });
+function cors(arg0: {
+  origin: string | undefined;
+  methods: string[];
+  credentials: boolean;
+}): any {
+  throw new Error("Function not implemented.");
+}
